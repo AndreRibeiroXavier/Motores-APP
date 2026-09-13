a723386168994105e79aff1ce99200d608896bb2
André <xavierandre840@gmail.com>
2026-09-13 13:46:05 -0300

    feat: projeto Angular inicial com environment e bootstrap configurado

---
+++ frontend/src/main.ts
@@ -0,0 +1,8 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { App } from './app/app';
+import { appConfig } from './app/app.config';
+
+bootstrapApplication(App, {
+  providers: [...appConfig.providers, provideHttpClient()],
+}).catch((err) => console.error(err));
